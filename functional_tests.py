c9ed48400ff334ef3fed0f57a5f535ae0ec8304c
henriquecdb <henriquecamposj@gmail.com>
2021-10-06 10:43:54 -0300

    Primeiro FT especificado nos comentários, utilizando agora o unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,38 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-assert 'Django' in browser.tittle
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith ouviu falar de uma nova aplicação online interessante para
+        # lista de tarefas. Ela decide verificar sua homepage
+        self.browser.get('http://localhost:8000')
+
+        # Ela percebe que o título da página e o cabeçalho mencionam
+        #  listas de tarefas (to-do)
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+# Ela é convidada a inserir um item de tarefa imediatamente
+
+# Ela digita "Buy peacock feathers" (Comprar penas de pavão) em uma caixa
+# de texto (o hobby de Edith é fazer iscas para pesca cmo fly)
+
+# Quando ela tecla enter, a página é atualizada, e agora a página lista
+# "1: Buy peacock feathers" como um item em uma lista de tarefas
+
+# Ainda continua havendo um caixa de texto convidando-a a acrescentar outro
+#  item. Ela insere "Use peacock feathers to make a fly" (Usar penas de pavão
+#  para fazer um fly - Edith é bem metódica)
+
+# Ela acessa esse URL - sua lista de tarefas continua lá;
+
+# Satisfeita, ela volta a dormir
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
