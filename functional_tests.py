53f1b9ac75ccd2909c189dcf062ece058e22cc79
henriquecdb <henriquecamposj@gmail.com>
2022-02-24 16:49:33 -0300

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -19,11 +19,11 @@ class NewVisitorTest(unittest.TestCase):
         # Ela percebe que o título da página e o cabeçalho mencionam
         #  listas de tarefas (to-do)
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element('h1').text
+        header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
         # Ela é convidada a inserir um item de tarefa imediatamente
-        inputbox = self.browser.find_element('id_new_item')
+        inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -38,10 +38,11 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element('id_list_table')
-        rows = table.find_elements('tr')
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # Ainda continua havendo um caixa de texto convidando-a a acrescentar outro
